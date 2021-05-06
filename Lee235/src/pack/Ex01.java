e11c12a72c949e31d2546d541dde7946c51a291d
이상오 <@DESKTOP-MFVLMB5>
2021-05-07 00:46:21 +0900

    20210507

---
+++ Lee235/src/pack/Ex01.java
@@ -0,0 +1,21 @@
+package pack;
+
+public class Ex01 {
+
+	
+	int d1;
+	int d2;
+	int g1;
+	int g2;
+	
+	public void Gugu(int d1,int d2,int g1,int g2) {
+		for(int i=d1;i<=d2;i++) {
+			for(int j=g1;j<=g2;j++) {
+				System.out.println(i*j);
+			}
+			System.out.println();
+		}
+		
+	}
+	
+}
---
+++ Lee235/src/pack/Ex02.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		Ex01 ex=new Ex01();
+		ex.Gugu(3, 4, 2, 6);
+		
+	}
+
+}
